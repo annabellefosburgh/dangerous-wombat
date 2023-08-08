847d225a3e2bf30d4ee5be10572b063eb972bfb8
annabellefosburgh <afosburgh20@gmail.com>
2023-08-07 20:49:27 -0500

    Writing login function

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,46 @@
+//Dependencies
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+
+import Auth from '../utils/auth';
+
+//Writing login functionality
+const Login = (props) => {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [login, { error, data }] = useMutation(LOGIN_USER);
+  
+    //Update state based on form input
+    const handleChange = (event) => {
+      const { name, value } = event.target;
+  
+      setFormState({
+        ...formState,
+        [name]: value,
+      });
+    };
+  
+    //Submitting form
+    const handleFormSubmit = async (event) => {
+      event.preventDefault();
+      console.log(formState);
+      try {
+        const { data } = await login({
+          variables: { ...formState },
+        });
+  
+        Auth.login(data.login.token);
+      } catch (e) {
+        console.error(e);
+      }
+  
+      //Clear Form
+      setFormState({
+        email: '',
+        password: '',
+      });
+    }
+};
+
+//Return html? render homepage?
