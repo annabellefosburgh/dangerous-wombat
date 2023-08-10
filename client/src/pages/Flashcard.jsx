065911c45c4bdbe96fc27fc77f55a6411b540fd3
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-08-09 19:30:39 -0500

    Added placeholder flashcard data.

--- client/src/pages/Flashcard.jsx
+++ client/src/pages/Flashcard.jsx
@@ -6,4 +6,40 @@ function Flashcard() {
     );
 }
 
+const SAMPLE_FLASHCARDS = [
+    {
+        id: 1,
+        question: 'Question 1?',
+        answer: 'Answer 1',
+        options: [
+            'Answer 1',
+            'Answer 2',
+            'Answer 3',
+            'Answer 4'
+        ]
+    },
+    {
+        id: 2,
+        question: 'Question 2?',
+        answer: 'Answer 2',
+        options: [
+            'Answer 1',
+            'Answer 2',
+            'Answer 3',
+            'Answer 4'
+        ]
+    },
+    {
+        id: 3,
+        question: 'Question 3?',
+        answer: 'Answer 3',
+        options: [
+            'Answer 1',
+            'Answer 2',
+            'Answer 3',
+            'Answer 4'
+        ]
+    }
+]
+
 export default Flashcard;
