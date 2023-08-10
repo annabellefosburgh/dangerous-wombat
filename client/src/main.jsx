5efaf1888632148620c7ac3212782a2e48a00e76
annabellefosburgh <afosburgh20@gmail.com>
2023-08-09 21:36:27 -0500

    Solving merge conflict in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,20 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom/dist'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from './App.jsx'
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
+import App from './App.jsx';
+import Home from './pages/Home.jsx';
+import Flashcard from './pages/Flashcard.jsx';
+import Quizzes from './pages/Quizzes.jsx'
+import Podcasts from './pages/Podcasts.jsx';
+import Signup from './pages/Signup.jsx';
+import Login from './pages/Login.jsx';
 
-
+// create browser router to handle page redirection
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
-    error: <Error />,
+    errorElement: <h1 className='display-2'>Wrong page!</h1>,
     children: [
       {
         index: true,
@@ -22,11 +25,21 @@ const router = createBrowserRouter([
       }, {
         path: '/signup',
         element: <Signup />
+      }, {
+        path: '/flashcards',
+        element: <Flashcard />
+      }, {
+        path: '/quizzes',
+        element: <Quizzes />
+      }, {
+        path: '/podcasts',
+        element: <Podcasts />
       }
     ]
-  }
-])
+  },
+]);
 
+// creates root element of the page with the router provider
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
-)
+);
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <nav className="navbar">
+        <a href="/">Logo</a>
+        <ul>
+          <li><a href="/flashcards">Flashcards</a></li>
+          <li><a href="/tests">Mock Tests</a></li>
+          <li><a href="/login">Login</a></li>
+        </ul>
+      </nav>
+      <main className="main-content">
+        <h1>Welcome to FlashPrep!</h1>
+        <p>Your personalized platform to prepare for interviews and tests using interactive flashcards.</p>
+        <a href="/login" className="get-started-btn">Get Started</a>
+      </main>
+      <footer className="footer">
+        <p>&copy; 2023 FlashPrep. All Rights Reserved.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
