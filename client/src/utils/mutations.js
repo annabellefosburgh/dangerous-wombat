ecda754b8fe3691d76b0e4373dca1a7883f066c0
John Henry Hopkins <jhhopkins13@gmail.com>
2023-08-10 21:04:53 -0500

    schema folder for server to run, not done

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,6 @@
 import { gql } from '@apollo/client';
 
+// Create a flashcard
 export const CREATE_FLASHCARD = gql`
   mutation CreateFlashcard($title: String!, $content: String!) {
     createFlashcard(title: $title, content: $content) {
@@ -10,6 +11,7 @@ export const CREATE_FLASHCARD = gql`
   }
 `;
 
+// Update a flashcard
 export const UPDATE_FLASHCARD = gql`
   mutation UpdateFlashcard($id: ID!, $title: String, $content: String) {
     updateFlashcard(id: $id, title: $title, content: $content) {
@@ -20,6 +22,7 @@ export const UPDATE_FLASHCARD = gql`
   }
 `;
 
+// Delete a flashcard
 export const DELETE_FLASHCARD = gql`
   mutation DeleteFlashcard($id: ID!) {
     deleteFlashcard(id: $id) {
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,8 +1,20 @@
 import { gql } from '@apollo/client';
 
+// Fetch all flashcards
 export const GET_FLASHCARDS = gql`
   query GetFlashcards {
     flashcards {
+      id
+      title
+      content
+    }
+  }
+`;
+
+// Fetch a single flashcard by id
+export const GET_FLASHCARD_BY_ID = gql`
+  query GetFlashcardById($id: ID!) {
+    flashcard(id: $id) {
       id
       title
       content
