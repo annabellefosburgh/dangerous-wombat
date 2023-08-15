3bdee2e25c916c2f31c69dfb9b59f8244ec9f901
John Henry Hopkins <jhhopkins13@gmail.com>
2023-08-14 20:28:55 -0400

    login and add profile

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -27,3 +27,26 @@ export const DELETE_FLASHCARD = gql`
     }
   }
 `;
+
+
+export const ADD_PROFILE = gql`
+  mutation AddProfile($username: String!, $email: String!) {
+    addProfile(username: $username, email: $email) {
+      id
+      username
+      email
+    }
+  }
+`;
+
+
+export const LOGIN_USER = gql`
+  mutation LoginUser($username: String!, $password: String!) {
+    loginUser(username: $username, password: $password) {
+      token
+      user {
+        // any fields you want to return
+      }
+    }
+  }
+`;
