bc1ddc7aaae4bb95c19722df779f418564725e10
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-08-16 02:39:59 -0500

    Added ability to mark flashcard answers as correct or incorrect.

--- client/src/components/Flashcards/FlashcardList.jsx
+++ client/src/components/Flashcards/FlashcardList.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Flashcard from './Card';
 
-// Takes a prop named 'flashcards'
 export default function FlashcardList({ flashcards }) {
+    // State to track the overall score for all flashcards
+    const [score, setScore] = useState(0);
+
+    // State to track the current flashcard index
+    const [currentCardIndex, setCurrentCardIndex] = useState(0);
+
+    // Function to handle when a user answers a flashcard
+    const handleAnswer = (isCorrect) => {
+        setScore(prevScore => isCorrect ? prevScore + 1 : prevScore);
+    };
+
+    // Function to go to the next flashcard
+    const handleNextCard = () => {
+        setCurrentCardIndex(prevIndex => prevIndex + 1);
+    };
+
     return (
         <div className="card-grid">
-            {flashcards.map(flashcard => {
-                // Map through each flashcard in the 'flashcards' array
-                // Render the Flashcard component for each flashcard
-                // Pass the 'flashcard' object as a prop, and set a 'key' for React's optimization
-                return <Flashcard flashcard={flashcard} key={flashcard.id} />
-            })}     
+            {flashcards.length > currentCardIndex && (
+                <Flashcard
+                    flashcard={flashcards[currentCardIndex]}
+                    onAnswer={handleAnswer}
+                    key={currentCardIndex}
+                />
+            )}
+            <div className="score">Overall Score: {score}</div>
+            {currentCardIndex < flashcards.length - 1 && (
+                <button onClick={handleNextCard}>Next</button>
+            )}
         </div>
-    )
+    );
 }
