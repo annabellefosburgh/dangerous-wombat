0461fce7db20dbd92cdf43c9b7791c74cbe3ee0c
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-08-18 15:18:06 -0500

    Resolved flashcards.

--- client/src/components/Flashcards/Card.jsx
+++ client/src/components/Flashcards/Card.jsx
@@ -1,40 +1,33 @@
 import React, { useState } from 'react';
 
 export default function Flashcard({ flashcard, onAnswer }) {
-    // State variable 'flip' to manage the flipping of the flashcard. Initial value is set to false.
+    // State to manage the flipping of the flashcard
     const [flip, setFlip] = useState(false);
 
-    // State variable 'answerStatus' to track whether the answer was marked as correct or incorrect. Initial value is null.
+    // State to track whether the answer was correct or incorrect
     const [answerStatus, setAnswerStatus] = useState(null);
 
-    // Function to handle marking the answer as correct or incorrect.
-    // It updates the 'answerStatus' state with the corresponding string and calls the 'onAnswer' callback with a boolean.
+    // Handle marking the answer as correct or incorrect
     const handleAnswer = (isCorrect) => {
         setAnswerStatus(isCorrect ? 'Correct' : 'Incorrect');
         onAnswer(isCorrect);
     };
 
     return (
-        // Container for the flashcard.
         <div className="card-container">
-            // Card element that flips when clicked. 
             <div
                 className={`card ${flip ? 'flip' : ''}`}
-                onClick={() => setFlip(!flip)} // Toggle the 'flip' state value when the card is clicked.
+                onClick={() => setFlip(!flip)}
             >
-                // Front side of the card showing the question.
                 <div className="front">{flashcard.question}</div>
-                // Back side of the card showing the answer.
                 <div className="back">{flashcard.answer}</div>
             </div>
-            // Buttons to mark the answer as correct or incorrect, displayed only when the card is flipped.
             {flip && (
                 <div className="buttons">
                     <button className="correct-button" onClick={() => handleAnswer(true)}>Correct</button>
                     <button className="incorrect-button" onClick={() => handleAnswer(false)}>Incorrect</button>
                 </div>
             )}
-            // Div element to display the status of the answer (Correct or Incorrect).
             <div className="answer-status">{answerStatus}</div>
         </div>
     );
